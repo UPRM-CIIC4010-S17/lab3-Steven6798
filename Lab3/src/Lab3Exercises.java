0581d7afe0220e57928a4054dd4dffc1d9ec511e
Steven A. Ramirez Rosa <Steven.ramirez3@upr.edu>
2017-12-06 15:33:43 -0400

    Created a flag using different figures.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Steven A. Ramírez Rosa");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setLocation(700, 300);
+		myFrame.setSize(500, 500);
 		
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -22,37 +22,77 @@ public class MyPanelClass extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.GRAY);
+		g.setColor(Color.WHITE);
 		g.fillRect(x1, y1, width + 1, height + 1);
 		
-		//Draw a border
-        g.setColor(Color.BLUE);
-        g.drawRect(x1, y1, width, height);
-      
-        //Draw a second border
+//		//Draw a border
+//        g.setColor(Color.BLUE);
+//        g.drawRect(x1, y1, width, height);
+//      
+//        //Draw a second border
+//        g.setColor(Color.RED);
+//        g.drawRect(x1 + 10, y1 + 10, width - 20, height - 20);
+//        
+//        //Draw a diagonal line
+//        g.setColor(Color.WHITE);
+//        g.drawLine(x1, y1, x2, y2);
+//        
+//        //Draw a oval
+//        g.setColor(Color.YELLOW);
+//        g.fillOval((x2 - 55)/ 2, (y2 - 55)/ 2, 55, 55);
+//
+//        
+//        Polygon p = new Polygon();
+//        p.addPoint(x1 + 5, y1 + 25);
+//        p.addPoint(x1 + 20, y1 + 10);
+//        p.addPoint(x1 + 35, y1 + 25);
+//        p.addPoint(x1 + 25, y1 + 25);
+//        p.addPoint(x1 + 25, y1 + 45);
+//        p.addPoint(x1 + 15, y1 + 45);
+//        p.addPoint(x1 + 15, y1 + 25);
+//        g.setColor(Color.CYAN);
+//        g.fillPolygon(p);
+//        
+//        Polygon p2 = new Polygon();
+//        p2.addPoint(x1 + 25, y1 + 73);
+//        p2.addPoint(x1 + 41, y1 + 73);
+//        p2.addPoint(x1 + 47, y1 + 58);
+//        p2.addPoint(x1 + 53, y1 + 73);
+//        p2.addPoint(x1 + 69, y1 + 73);
+//        p2.addPoint(x1 + 56, y1 + 83);
+//        p2.addPoint(x1 + 61, y1 + 98);
+//        p2.addPoint(x1 + 47, y1 + 88);
+//        p2.addPoint(x1 + 34, y1 + 98);
+//        p2.addPoint(x1 + 38, y1 + 83);
+//        g.setColor(Color.GREEN);
+//        g.drawPolygon(p2);
+        
+        // Flag divisions
         g.setColor(Color.RED);
-        g.drawRect(x1 + 10, y1 + 10, width - 20, height - 20);
-        
-        //Draw a diagonal line
+        g.fillRect(x1 + 10, y1 + 10, x1 + 210, y1 + 150);
         g.setColor(Color.WHITE);
-        g.drawLine(x1, y1, x2, y2);
+        g.fillRect(x1 + 10, y1 + 10 + (130 / 5), x1 + 210, y1 + 150/2);
+        g.setColor(Color.RED);
+        g.fillRect(x1 + 10, y1 + 10 + (135 * 2/ 5), x1 + 210, y1 + 150/3);
+        g.setColor(Color.WHITE);
+        g.fillRect(x1 + 10, y1 + 10 + (145 * 3/ 5), x1 + 210, y1 + 150/4);
+        g.setColor(Color.RED);
+        g.fillRect(x1 + 10, y1 + 10 + (150 * 4/ 5), x1 + 210, y1 + 150/5);
+        g.setColor(Color.BLACK);
         
-        //Draw a oval
-        g.setColor(Color.YELLOW);
-        g.fillOval((x2 - 55)/ 2, (y2 - 55)/ 2, 55, 55);
-
+        // Flag rectangle
+     	g.setColor(Color.BLACK);
+        g.drawRect(x1 + 10, y1 + 10, x1 + 210, y1 + 150);
         
-        Polygon p = new Polygon();
-        p.addPoint(x1 + 5, y1 + 25);
-        p.addPoint(x1 + 20, y1 + 10);
-        p.addPoint(x1 + 35, y1 + 25);
-        p.addPoint(x1 + 25, y1 + 25);
-        p.addPoint(x1 + 25, y1 + 45);
-        p.addPoint(x1 + 15, y1 + 45);
-        p.addPoint(x1 + 15, y1 + 25);
-        g.setColor(Color.YELLOW);
-        g.fillPolygon(p);
+        // Flag triangle
+        Polygon triangle = new Polygon();
+        triangle.addPoint(x1 + 10, y1 + 10);
+        triangle.addPoint((x1 + 10) + (200/2), (y1 + 10) + (140 / 2));
+        triangle.addPoint(x1 + 10, 2 * y1 + 160);
+        g.setColor(Color.BLUE);
+        g.fillPolygon(triangle);
         
+        // Flag star
         Polygon p2 = new Polygon();
         p2.addPoint(x1 + 25, y1 + 73);
         p2.addPoint(x1 + 41, y1 + 73);
@@ -65,6 +105,6 @@ public class MyPanelClass extends JPanel {
         p2.addPoint(x1 + 34, y1 + 98);
         p2.addPoint(x1 + 38, y1 + 83);
         g.setColor(Color.WHITE);
-        g.drawPolygon(p2);
+        g.fillPolygon(p2);
 	}
 }
